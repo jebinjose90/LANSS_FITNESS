6b576d3774b614a9c342c2da146d13584cab30d1
Jebin Jose Antony <jebinjoseantony@Jebins-MacBook-Air.local>
2024-10-28 05:37:30 +0530

    my 11th update

--- frontend/src/modules/user/pages/UserLogin.tsx
+++ frontend/src/modules/user/pages/UserLogin.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AuthenticationUISkin from '../../common/AuthenticationUISkin';
 import Login from '../../common/authenticationComponents/Login';
+import { useUserAuth } from '../hooks/manageUserAuth';
 
 const UserLogin: React.FC = () => {
+const { loading, error, login } = useUserAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    login(email, password);
+  };
     return (
         <AuthenticationUISkin
         imageUrl="https://i.imgur.com/9MJtr4G.jpeg"
@@ -11,7 +20,7 @@ const UserLogin: React.FC = () => {
         showUserSignup = {true}
         userSigupText = 'New User?'
         userSignupHref = ''
-        onSubmit={() => console.log('Submitted')}>
+        onSubmit={() => handleSubmit}>
             <Login />
         </AuthenticationUISkin>
     )
