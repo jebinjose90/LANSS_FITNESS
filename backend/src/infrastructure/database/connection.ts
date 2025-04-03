0a1c16323d06e4811d716f48fa4606474b0a6f6b
jebinjose90 <jebinjose.sics@gmail.com>
2025-04-04 00:31:04 +0530

    Some old updates have been updated back.

--- backend/src/infrastructure/database/connection.ts
+++ backend/src/infrastructure/database/connection.ts
@@ -5,9 +5,29 @@ import mongoose from 'mongoose';
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/lanss-fitness');
-    console.log('Connected to MongoDB successfully');
+
+    const db = mongoose.connection;
+    console.log(⁠`✅ MongoDB connected successfully: ${db.host}:${db.port}/${db.name}`);
+
+    db.on('connected', () => {
+      console.log(`✅ MongoDB connected successfully: ${db.host}:${db.port}/${db.name}`);
+    });
+
+    db.on('error', (err) => {
+      console.error(`❌ MongoDB connection error: ${err.message}`);
+      process.exit(1); // Exit the process on connection failure
+    });
+
+    db.on('disconnected', () => {
+      console.warn('⚠️ MongoDB disconnected. Reconnecting...');
+    });
+
+    db.on('reconnected', () => {
+      console.log('🔄 MongoDB reconnected successfully.');
+    });
+
   } catch (err) {
-    console.error('Failed to connect to MongoDB:', err);
+    console.error('❌ Failed to connect to MongoDB:', err);
     process.exit(1); // Exit the process on connection failure
   }
 };
